2f3f44e44aaf0734e4f83c223a47cf9b76167fda
pirainogi <pirainogi@gmail.com>
2019-07-21 22:18:55 -0400

    home pg css

--- src/components/home.js
+++ src/components/home.js
@@ -6,12 +6,20 @@ const Home = () => {
   return(
     <div className="container">
       <div className="tophalf">
-        <form>
+
+        <form className="loginForm">
+          <input type="username" name="username" value={null} placeholder="Username"/>
+          <input type="password" name="password" value={null} placeholder="Password"/>
+          <input type="submit" value="Login" className="button"/>
         </form>
+
+        <p>Forgot Password?</p>
         <h1> Join the Garden Party</h1>
-        <p> TAGLINE TAGLINE TAGLINE</p>
+        <h3> TAGLINE TAGLINE TAGLINE</h3>
       </div>
       <div className="bottomhalf">
+        <button>Login</button>
+        <button>Signup</button>
       </div>
     </div>
   )
