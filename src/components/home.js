9a258a77d40271f86c9ef7e373a7c516d55f61b0
pirainogi <pirainogi@gmail.com>
2019-07-21 21:56:56 -0400

    home pg css

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import '../css/home.css'
 
 const Home = () => {
 
   return(
-    <div>
-      <h1> I am the Home page</h1>
+    <div className="container">
+      <div className="tophalf">
+        <form>
+        </form>
+        <h1> Join the Garden Party</h1>
+        <p> TAGLINE TAGLINE TAGLINE</p>
+      </div>
+      <div className="bottomhalf">
+      </div>
     </div>
   )
 }
