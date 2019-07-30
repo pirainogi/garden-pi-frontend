b6710f0eef3fb6746e92920493a61eb3bc04990d
pirainogi <pirainogi@gmail.com>
2019-07-30 15:51:26 -0400

    can login from home

--- src/components/home.js
+++ src/components/home.js
@@ -1,33 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../actions/actions'
+
 import '../css/home.css'
 import Footer from './footer'
 
-const Home = () => {
+class Home extends Component {
 
-  return(
-    <div>
-      <div className="container">
-        <div className="tophalf">
+  state = {
+    email: '',
+    password: ''
+  }
 
-          <form className="loginForm">
-            <input type="username" name="username" value={null} placeholder="Username"/>
-            <input type="password" name="password" value={null} placeholder="Password"/>
-            <input type="submit" value="Login" className="button"/>
-          </form>
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-          <p>Forgot Password?</p>
-          <h1> Join the Garden Party</h1>
-          <h3> TAGLINE TAGLINE TAGLINE </h3>
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('sending the login fetch from home');
+    this.props.loginUser(this.state)
+    this.props.history.push('/profile')
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="container">
+          <div className="tophalf">
+
+            <form onSubmit={this.handleSubmit} className="loginForm">
+              <input type="email" name="email" onChange={this.handleChange} value={this.state.email} placeholder="Email"/>
+              <input type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password"/>
+              <input type="submit" value="Login" className="button"/>
+            </form>
+
+            <p>Forgot Password?</p>
+            <h1> Join the Garden Party</h1>
+            <h3> TAGLINE TAGLINE TAGLINE </h3>
+          </div>
+          <div className="bottomhalf">
+            <NavLink to="/login" activeClassName="active" id="navlink">Login</NavLink>
+            <NavLink to="/signup" activeClassName="active" id="navlink">Signup</NavLink>
+          </div>
         </div>
-        <div className="bottomhalf">
-          <NavLink to="/login" activeClassName="active" id="navlink">Login</NavLink>
-          <NavLink to="/signup" activeClassName="active" id="navlink">Signup</NavLink>
-        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  )
+    )
+  }
+
+}
+const mapStateToProps = (state) => {
+  console.log('home state', state);
+  return {
+    state
+  }
 }
 
-export default Home;
+export default connect(mapStateToProps, actions)(withRouter(Home));
