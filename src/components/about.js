663effcf55c0c41e099ae8c525e6798f4f1edc39
pirainogi <pirainogi@gmail.com>
2019-07-31 11:36:24 -0400

    about

---
+++ src/components/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header';
+import Footer from './footer';
+
+const About = () => {
+
+  return(
+    <div>
+      <Header />
+      <h1>I am the About Page</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default About;
