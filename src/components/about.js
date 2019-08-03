4e7bd9a89632b3a3eab00683aa2eacd3fc8b018d
pirainogi <pirainogi@gmail.com>
2019-08-03 09:40:10 -0400

    css css css

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import '../css/about.css'
 
 const About = () => {
 
   return(
-    <div>
+    <div className="about">
       <Header />
       <h1>I am the About Page</h1>
       <Footer />
