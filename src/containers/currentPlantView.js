ac662c683d18d46b3357a506cb4384ac5aa3b594
pirainogi <pirainogi@gmail.com>
2019-08-05 06:44:23 -0400

    plant view getting there

--- src/containers/currentPlantView.js
+++ src/containers/currentPlantView.js
@@ -7,8 +7,21 @@ const CurrentPlantView = () => {
 
   return(
     <div className="currentPlantView">
-      <h1>This is where information about a clicked plant will live and provide plant health information hurray</h1>
-      <HealthMeter/>
+      <div className='upper-plant-view'>
+        <div>
+          <p>plant name</p>
+          <div className='plant-pic'></div>
+        </div>
+        <div>
+          <p>family info here</p>
+          <p>age of plant</p>
+          <HealthMeter/>
+        </div>
+        <div>
+          <p>group name</p>
+          <button>remove plant</button>
+        </div>
+      </div>
       <ToDo/>
     </div>
   )
