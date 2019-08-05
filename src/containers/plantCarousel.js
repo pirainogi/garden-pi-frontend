392d09d1117564a6f742a36d98162678ec687a7e
pirainogi <pirainogi@gmail.com>
2019-08-04 22:25:11 -0400

    plant card

--- src/containers/plantCarousel.js
+++ src/containers/plantCarousel.js
@@ -6,7 +6,17 @@ const PlantCarousel = () => {
 
   return(
     <div className="plantCarousel">
-      <span>This is where a scrolly bar of plants will live (I am a container)</span>
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
+      <PlantCard />
       <PlantCard />
       <PlantCard />
       <PlantCard />
