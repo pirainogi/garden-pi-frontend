2169a35d554e8d89404681c3829137e67c08074f
pirainogi <pirainogi@gmail.com>
2019-08-06 16:54:02 -0400

    added header to signup page

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import SignupForm from '../components/signupForm'
+import Header from '../components/header';
+import SignupForm from '../components/signupForm';
 import '../css/signup.css';
 
 const Signup = () => {
 
   return(
     <div className="signup">
+      <Header />
       <h1>I am the Signup Page</h1>
       <SignupForm />
     </div>
