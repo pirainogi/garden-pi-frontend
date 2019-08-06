6f52c3e436ab0a2f7053b1c4767d79be23f2a8ca
pirainogi <pirainogi@gmail.com>
2019-08-06 15:06:42 -0400

    saving old code

--- src/containers/currentPlantView.js
+++ src/containers/currentPlantView.js
@@ -8,14 +8,16 @@ const CurrentPlantView = () => {
   return(
     <div className="currentPlantView">
       <div className='upper-plant-view'>
-        <div>
-          <p>plant name</p>
-          <div className='plant-pic'></div>
-        </div>
-        <div>
-          <p>family info here</p>
-          <p>age of plant</p>
-          <HealthMeter/>
+        <div className='left-plant-view'>
+          <div className='plant-basics'>
+            <p>plant name</p>
+            <div className='plant-pic'></div>
+          </div>
+          <div>
+            <p>family info here</p>
+            <p>age of plant</p>
+            <HealthMeter/>
+          </div>
         </div>
         <div>
           <p>group name</p>
