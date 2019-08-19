9c1d2976cd143bc9717b6e5025292ba22a5ae722
pirainogi <pirainogi@gmail.com>
2019-08-19 12:50:00 -0400

    grabbing the scientific names

--- src/components/plantCard.js
+++ src/components/plantCard.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLeaf } from '@fortawesome/free-solid-svg-icons'
 
 import '../css/plantCard.css';
 
-const PlantCard = (props) => {
-  // console.log(props);
+class PlantCard extends Component {
+
+  state = {
+    species: ''
+  }
+
+  componentDidMount(){
+    if(this.props.plant){
+      fetch(`http://localhost:3000/api/v1/species/${this.props.plant.species_id}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          species: data.scientific_name
+        })
+      })
+    }
+  }
+
+  render(){
     return(
       <div className="plantCard">
-        {props.plant
+        {this.props.plant
           ? <div>
-            <span>{props.plant.name}</span><br></br>
-            <span>Species:{props.plant.species_id}</span><br></br>
-            <FontAwesomeIcon icon={faLeaf} size='3x' color={'rgb(150, 171, 108)'}/>
+            <span className='plantName'>{this.props.plant.name}</span><br></br>
+            <span>Species: {this.state.species}</span><br></br>
+            <FontAwesomeIcon icon={faLeaf} size='1x' color={'rgb(150, 171, 108)'}/>
           </div>
           : <span>Plant Card</span>
         }
       </div>
     )
+  }
 
 } // end of class
 
