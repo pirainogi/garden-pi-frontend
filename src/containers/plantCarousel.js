e2bbb81227b82e5b81a3c1e53c232c0db12aba2c
pirainogi <pirainogi@gmail.com>
2019-08-19 08:53:53 -0400

    plants render in the carousel

--- src/containers/plantCarousel.js
+++ src/containers/plantCarousel.js
@@ -3,35 +3,24 @@ import { connect } from 'react-redux';
 import PlantCard from '../components/plantCard'
 import '../css/plantCarousel.css'
 
-const PlantCarousel = () => {
-  //
-  // renderPlants = () => {
-  //   if (this.props.state.currentUser) {
-  //     return this.props.state.currentUser.groups.map(group => {
-  //       return group.plants.map(plant => {
-  //         // console.log(plant);
-  //         return <PlantCard key={plant.id} plant={plant}/>
-  //       })
-  //     })
-  //   }
-  // }
+const PlantCarousel = (props) => {
+
+  function renderPlants(){
+    if (props.state.currentUser) {
+      return props.state.currentUser.groups.map(group => {
+        return group.plants.map(plant => {
+          // console.log(plant);
+          return <PlantCard key={plant.id} plant={plant}/>
+        })
+      })
+    }
+  }
+
+  console.log(props);
 
   return(
     <div className="plantCarousel">
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
-      <PlantCard />
+      {renderPlants()}
     </div>
   )
 }
