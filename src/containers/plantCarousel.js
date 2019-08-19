83691cf601b07163d0910fbd86108b5d7cd77f6c
pirainogi <pirainogi@gmail.com>
2019-08-19 08:50:08 -0400

    added redux to plantCarousel

--- src/containers/plantCarousel.js
+++ src/containers/plantCarousel.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PlantCard from '../components/plantCard'
 import '../css/plantCarousel.css'
 
 const PlantCarousel = () => {
+  //
+  // renderPlants = () => {
+  //   if (this.props.state.currentUser) {
+  //     return this.props.state.currentUser.groups.map(group => {
+  //       return group.plants.map(plant => {
+  //         // console.log(plant);
+  //         return <PlantCard key={plant.id} plant={plant}/>
+  //       })
+  //     })
+  //   }
+  // }
 
   return(
     <div className="plantCarousel">
@@ -24,4 +36,11 @@ const PlantCarousel = () => {
   )
 }
 
-export default PlantCarousel;
+const mapStateToProps = (state) => {
+  // console.log(`user's plants`, state);
+  return {
+    state
+  }
+}
+
+export default connect(mapStateToProps)(PlantCarousel);
