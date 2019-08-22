dfcca862aa13dc1c5b2e62982d7be2663a50c562
pirainogi <pirainogi@gmail.com>
2019-08-22 12:30:18 -0400

    trying to get the edit user form in a modal broken

--- src/components/editUser.js
+++ src/components/editUser.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions/actions'
 import EditUserForm from '../components/editUserForm';
-import EditPasswordForm from '../components/editPasswordForm';
+import Modal from './modal';
+import '../css/modal.css'
+// import EditPasswordForm from '../components/editPasswordForm';
 
 import '../css/editUser.css'
 
 class EditUser extends Component {
 
-  editUser = () => {
-    console.log('click clack edit a user');
+  openCloseModal = () => {
+    console.log('clicking btn');
+    this.props.toggleModal()
   }
 
-  editPassword = () => {
-    console.log('click clack edit pw');
-  }
+  // editUser = () => {
+  //   console.log('click clack edit a user');
+  // }
+  //
+  // editPassword = () => {
+  //   console.log('click clack edit pw');
+  // }
 
   render(){
-    // console.log('edit user render', this.props);
+    // console.log('edit user render', this.props.state.showModal);
+    const modal = this.props.state.showModal
+    ? (
+      <Modal>
+        <div id="outer-modal">
+          <div className="inner-modal">
+          <div className="modal-content">
+          <EditUser />
+          <button onClick={this.openCloseModal}>hide</button></div></div>
+        </div>
+      </Modal>
+    )
+    : null
     return(
       <div className="editUser">
-        <button className="edit-user-btn" onClick={this.editUser}>click me to edit user</button>
-        <button className="edit-pw-btn" onClick={this.editPassword}>click me to edit pw</button>
-        <EditUserForm />
-        <EditPasswordForm />
+        <button className="edit-user-btn" onClick={this.openCloseModal}>click me to edit user</button>
+        {modal}
       </div>
     )
   }
@@ -30,10 +48,9 @@ class EditUser extends Component {
 } // end of class
 
 const mapStateToProps = (state) => {
-  // console.log('user info', state);
   return {
     state
   }
 }
 
-export default connect(mapStateToProps)(EditUser);
+export default connect(mapStateToProps, actions)(EditUser);
