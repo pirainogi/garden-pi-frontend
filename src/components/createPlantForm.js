dcf5472ccf73cc0e387a9b1d54551065415670f5
pirainogi <pirainogi@gmail.com>
2019-08-22 13:11:00 -0400

    create plant modal but it opens both modals; need logic to only open one at a time

---
+++ src/components/createPlantForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/actions';
+import '../css/editUserForm.css';
+
+class CreatePlantForm extends Component {
+
+  state = {
+    groups: [],
+    species: [],
+    name: '',
+    species_id: '',
+    group_id: '',
+    humidity_min: '',
+    humidity_max: '',
+    ph_min: '',
+    ph_max: '',
+    soil_moisture_min: '',
+    soil_moisture_max: '',
+    temperature_min: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log('sending the edit user fetch');
+    // this.props.editUser(this.state)
+    this.props.toggleModal()
+  }
+
+  render(){
+    return(
+      <div className="createPlantForm">
+        <h1>make new plant one day</h1>
+      </div>
+    )
+  }
+
+} // end of class
+
+const mapStateToProps = (state) => {
+  return {
+    state
+  }
+}
+
+export default connect(mapStateToProps, actions)(CreatePlantForm);
--- src/containers/userPlants.js
+++ src/containers/userPlants.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions/actions';
+import Modal from '../components/modal';
 import PlantCard from '../components/plantCard';
+import CreatePlantForm from '../components/createPlantForm';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import '../css/modal.css'
 import '../css/userPlants.css';
 
 class UserPlants extends Component {
 
-  createPlant = () => {
-    console.log('clicking the create plant button');
+  // createPlant = () => {
+  //   console.log('clicking the create plant button');
+  // }
+
+  openCloseModal = () => {
+    console.log('clicking btn');
+    this.props.toggleModal()
   }
 
   renderPlants = () => {
@@ -22,13 +33,29 @@ class UserPlants extends Component {
 
   render(){
     // console.log('user info render', this.props.state.currentUser);
+    const modal = this.props.state.showModal
+    ? (
+      <Modal>
+        <div id="outer-modal">
+          <div className="inner-modal">
+            <div className="modal-content">
+            <button onClick={this.openCloseModal}><FontAwesomeIcon icon={faTimes} size='1x' color={'rgb(150, 171, 108)'}/></button>
+            <CreatePlantForm />
+            </div>
+          </div>
+        </div>
+      </Modal>
+    )
+    : null
+
     return(
       <div className="userPlants">
         <div className='user-plant-title'>
           <span>My Plants</span>
         </div>
         <div className='create-plant-row'>
-          <button className='create-plant-btn' onClick={this.createPlant}>Create New Plant</button>
+          <button className='create-plant-btn' onClick={this.openCloseModal}>Create New Plant</button>
+          {modal}
         </div>
         <div className='plant-card-container'>
         {this.renderPlants()}
@@ -40,10 +67,9 @@ class UserPlants extends Component {
 } // end of class
 
 const mapStateToProps = (state) => {
-  // console.log(`user's plants`, state);
   return {
     state
   }
 }
 
-export default connect(mapStateToProps)(UserPlants);
+export default connect(mapStateToProps, actions)(UserPlants);
