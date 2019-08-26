9902aa364595d14ebfe9fec607ca28a070d60ddc
pirainogi <pirainogi@gmail.com>
2019-08-26 15:34:29 -0400

    settimeout for icon color mf

--- src/components/actionCard.js
+++ src/components/actionCard.js
@@ -1,49 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTint, faCut, faShoppingBasket, faSun, faCloud,    faPoop, faCannabis, faArchive, faSeedling, faTractor } from '@fortawesome/free-solid-svg-icons'
 import '../css/actionCard.css'
 
-const ActionCard = (props) => {
+class ActionCard extends Component {
 
-  function selectIcon(){
-    switch(props.action.action_type){
+  state = {
+    color: 'rgb(150, 171, 108)'
+  }
+
+  selectIcon = () => {
+    switch(this.props.action.action_type){
       case 'Water':
-        return <FontAwesomeIcon icon={faTint} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faTint} size='3x' color={this.state.color}/>
       case 'Prune':
-        return <FontAwesomeIcon icon={faCut} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faCut} size='3x' color={this.state.color}/>
       case 'Harvest':
-        return <FontAwesomeIcon icon={faShoppingBasket} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faShoppingBasket} size='3x' color={this.state.color}/>
       case 'Sun':
-        return <FontAwesomeIcon icon={faSun} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faSun} size='3x' color={this.state.color}/>
       case 'Shade':
-        return <FontAwesomeIcon icon={faCloud} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faCloud} size='3x' color={this.state.color}/>
       case 'Fertilize':
-        return <FontAwesomeIcon icon={faPoop} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faPoop} size='3x' color={this.state.color}/>
       case 'Weed':
-        return <FontAwesomeIcon icon={faCannabis} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faCannabis} size='3x' color={this.state.color}/>
       case 'Transplant':
-        return <FontAwesomeIcon icon={faArchive} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faArchive} size='3x' color={this.state.color}/>
       case 'Deadhead':
-        return <FontAwesomeIcon icon={faSeedling} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faSeedling} size='3x' color={this.state.color}/>
       case 'Mulch':
-        return <FontAwesomeIcon icon={faTractor} size='3x' color={'rgb(150, 171, 108)'}/>
+        return <FontAwesomeIcon icon={faTractor} size='3x' color={this.state.color}/>
       default:
         return null
     }
   }
 
+  clickHandler = () => {
+    console.log(this.props.action.action_type);
+    this.setState({
+      color: 'rgb(0, 0, 0)'
+    })
+    setTimeout(() => {
+      this.setState({
+        color: 'rgb(150, 171, 108)'
+      })
+    }, 2000)
+  }
+
+
+  render(){
+    console.log(this.props)
     return(
       <div className="actionCard">
-        {props.action
-          ? <div>
-            <span>{props.action.action_type}</span><br></br><br></br>
-            {selectIcon()}
+        {this.props.action
+          ? <div onClick={this.clickHandler}>
+            <span>{this.props.action.action_type}</span><br></br><br></br>
+            {this.selectIcon()}
           </div>
 
           : <span>action card </span>
         }
       </div>
     )
+  }
+
 
 } // end of class
 
