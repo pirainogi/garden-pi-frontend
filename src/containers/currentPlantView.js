020738d76df8423e63830f9b56e3d4ef98f6260f
pirainogi <pirainogi@gmail.com>
2019-08-28 14:16:07 -0400

    changed currplantview to class component

--- src/containers/currentPlantView.js
+++ src/containers/currentPlantView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/actions';
 import Modal from '../components/modal';
@@ -11,73 +11,79 @@ import { faPagelines } from '@fortawesome/free-brands-svg-icons';
 import '../css/modal.css';
 import '../css/currentPlantView.css'
 
-const CurrentPlantView = (props) => {
+class CurrentPlantView extends Component {
 
-  function groupName(){
-    if(props.state.currentPlant){
-      let foundGroup = props.state.currentUser.groups.find(group =>
-        group.plants.find(plant => plant.id === props.state.currentPlant.id))
+  groupName = () => {
+    if(this.props.state.currentPlant){
+      let foundGroup = this.props.state.currentUser.groups.find(group =>
+        group.plants.find(plant => plant.id === this.props.state.currentPlant.id))
       return foundGroup.name
     }
   }
 
-  let openCloseModal = () => {
-    props.toggleModal(props.state.showModal ? null : 'DeletePlant')
+  openCloseModal = () => {
+    this.props.toggleModal(this.props.state.showModal ? null : 'DeletePlant')
   }
 
-  const modal = props.state.currentModal === 'DeletePlant'
-  ? (
-    <Modal>
-      <div id="outer-modal">
-        <div className="inner-modal">
-          <div className="modal-content">
-          <div className='topbox-modal'>
-            <button onClick={() => openCloseModal()}><FontAwesomeIcon icon={faTimes} size='1x' color={'rgb(150, 171, 108)'}/></button>
-          </div>
-          <DeletePlantForm />
-          </div>
-        </div>
-      </div>
-    </Modal>
-  )
-  : null
 
-  return(
-    <div>{ props.state.currentPlant
-      ?
-      <div className="currentPlantView">
-        <div className='upper-plant-view'>
-          <div className='left-plant-view'>
-            <div className='plant-basics'>
-              <p>{props.state.currentPlant.name}</p>
-              <div className='plant-pic'><span className='icon'><FontAwesomeIcon icon={faPagelines} size='5x' color={'white'}/></span></div>
+
+  render(){
+
+    const modal = this.props.state.currentModal === 'DeletePlant'
+    ? (
+      <Modal>
+        <div id="outer-modal">
+          <div className="inner-modal">
+            <div className="modal-content">
+            <div className='topbox-modal'>
+              <button onClick={this.openCloseModal}><FontAwesomeIcon icon={faTimes} size='1x' color={'rgb(150, 171, 108)'}/></button>
             </div>
-            <div className='plant-health'>
-              <p>family info here</p>
-              <p>age of plant</p>
+            <DeletePlantForm />
             </div>
-          </div>
-          <div className='right-plant-view'>
-            {props.state.currentPlant ? <p className='garden-group-name'>{groupName()}</p> : <p className='garden-group-name'>group name</p>}
-            <button className='remove-plant-btn' onClick={() => openCloseModal()}>remove plant</button>
-            <button className='data-tab'>data</button>
-            <button className='info-tab'>info</button>
-            {modal}
           </div>
         </div>
-      </div>
-      :
-      <div className="currentPlantView">
-        <div className='upper-plant-view'>
-          <div className='unpop-plant-view'>
-            <p>Click on a Plant to View Details</p>
+      </Modal>
+    )
+    : null
+
+
+    return(
+      <div>{ this.props.state.currentPlant
+        ?
+        <div className="currentPlantView">
+          <div className='upper-plant-view'>
+            <div className='left-plant-view'>
+              <div className='plant-basics'>
+                <p>{this.props.state.currentPlant.name}</p>
+                <div className='plant-pic'><span className='icon'><FontAwesomeIcon icon={faPagelines} size='5x' color={'white'}/></span></div>
+              </div>
+              <div className='plant-health'>
+                <p>family info here</p>
+                <p>age of plant</p>
+              </div>
+            </div>
+            <div className='right-plant-view'>
+              {this.props.state.currentPlant ? <p className='garden-group-name'>{this.groupName()}</p> : <p className='garden-group-name'>group name</p>}
+              <button className='remove-plant-btn' onClick={this.openCloseModal}>remove plant</button>
+              <button className='data-tab'>data</button>
+              <button className='info-tab'>info</button>
+              {modal}
+            </div>
           </div>
         </div>
+        :
+        <div className="currentPlantView">
+          <div className='upper-plant-view'>
+            <div className='unpop-plant-view'>
+              <p>Click on a Plant to View Details</p>
+            </div>
+          </div>
+        </div>
+      }
+        <ToDo/>
       </div>
-    }
-      <ToDo/>
-    </div>
-  )
+    )
+  }
 
 } // end of class
 
